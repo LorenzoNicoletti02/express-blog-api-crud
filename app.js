ae8c4828ab7aecba64ea87e0d12ed504567c88c1
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2024-12-06 16:20:03 +0100

    feat: post method

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const post = require("./data/posts");
 const postRouter = require("./routers/routersPosts");
 
+app.use(express.json());
 app.use(express.static("public"));
 app.use("/posts", postRouter);
 
--- controller/postController.js
+++ controller/postController.js
@@ -1,12 +1,14 @@
 const postList = require("../data/posts");
 
+// Index
 const index = (req, res) => {
     res.json(postList);
 };
 
+// Show
 const show = (req, res) => {
     let foundPost = null;
-    console.log(postList);
+    // console.log(postList);
     
     for (let i = 0; i < postList.length; i++) {
         const curElem = postList[i];
@@ -14,26 +16,44 @@ const show = (req, res) => {
             foundPost = curElem;
         }
     }
-    if (foundPost === null) {
-        res.sendStatus(404);
-    } else {
-        res.json(foundPost);
-    }
+    res.json(foundPost);
 }
 
-const create = (req, res) => {
-    res.json(`Modifica dei dati del post numero`);
+// Post
+const create = (req, res) => { // Il post deve aggiungere un oggetto all'array. Qusto oggetto è nel body della richiesta API.
+    const bodyReq = req.body;
+
+    // Prendo l'ultimo indice dell'array
+    const lastElemIndex = postList.length - 1;
+
+    // Prendo l'ultimo oggetto dall'array
+    const lastObj = postList[lastElemIndex];
+
+    // Prendo l'ID di questo ultimo oggetto 
+    const lastObjId = lastObj.id;
+
+    // Aggiungo la proprietà ID all'oggetto nuovo preso dall'API (postman)
+    bodyReq.id = lastObjId + 1;
+
+    // Pusho il nuovo ultimo oggetto nell'array
+    postList.push(bodyReq);
+
+    // Il jason di risposta sarà l'array modificato
+    res.json(postList);
 }
 
+// Put
 const update = (req, res) => {
     res.json(`Modifca del curPosto del post numero`);
 };
 
+// Patch
 const modify = (req, res) => {
     const postId = req.params.id;
     res.json(`Modifica del curPosto del post numero ${postId}`);
 }
 
+// Delete
 const destroy = (req, res) => {
     const postId = req.params.id;
     res.json(`Cancellazione del post numero ${postId}`);
