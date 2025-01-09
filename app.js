0cc566661a0178fbbd229c1f7e8ba15673daa8cf
LorenzoNicoletti02 <lorenzo.nicoletti02@gmail.com>
2025-01-09 17:56:27 +0100

    feat: fix bug

--- app.js
+++ app.js
@@ -3,22 +3,24 @@ const app = express();
 const port = 3000;
 const post = require("./data/posts");
 const postRouter = require("./routers/routersPosts");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/posts", postRouter);
 
 app.get("/", (req, res) => {
-    res.json("hello")
-})
+  res.json("hello");
+});
 
 app.get(`/bacheca`, (req, res) => {
-    res.json ({
-        posts: post,
-        length: post.length
-    })
+  res.json({
+    posts: post,
+    length: post.length,
+  });
 });
 
 app.listen(port, () => {
-    console.log(("server on"));
-})
+  console.log("server on");
+});
